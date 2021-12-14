10b8017dc5ab1e448d667ab4471094845c629b75
morsej <90578267+morsej@users.noreply.github.com>
2021-12-14 08:51:51 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,26 +4,18 @@
       // listen for changes
       inputs.forEach(input => input.addEventListener('change', handleUpdate));
       inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
-	  
-	    const stylesheet = document.styleSheets[1];
-			let boxParaRule;
-
-			for(let i = 0; i < stylesheet.cssRules.length; i++) {
-			  if(stylesheet.cssRules[i].selectorText === '.moon') {
-				boxParaRule = stylesheet.cssRules[i];
-			  }
-			}
 
       function handleUpdate(e) {
 		
-		  
+		var moon = document.getElementById('moon');
         // append 'px' to the end of spacing and blur variables
 		var amount = this.value;
 		if(amount > 50){
 			amount = amount - ((amount - 50) * 2)
-			boxParaRule.moon.style.setProperty('transform', 'scale(-2)');
+			
+			moon.setAttribute('style', 'transform: scale(-2);');
 		}else{
-			boxParaRule.style.setProperty('transform', 'scale(2)');
+			moon.setAttribute('style', 'transform: scale(2);');
 		}
 		  
 		const negValue = (amount * -1);
