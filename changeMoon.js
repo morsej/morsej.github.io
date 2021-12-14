0e532c854b6c308d2486ba6d55604f177cb3555b
morsej <90578267+morsej@users.noreply.github.com>
2021-12-14 13:22:00 -0500

    Add files via upload

---
+++ changeMoon.js
@@ -0,0 +1,89 @@
+// get the inputs
+      const inputs = [].slice.call(document.querySelectorAll('.controls input'));
+
+      // listen for changes
+      inputs.forEach(input => input.addEventListener('change', handleUpdate));
+      inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+      function handleUpdate(e) {
+		var moon = document.getElementById('adjustMoon');
+        // append 'px' to the end of spacing and blur variables
+		var amount = this.value;
+		if(amount > 50 && this.id != 'base'){
+			amount = amount - ((amount - 50) * 2)
+			
+			moon.setAttribute('style', 'transform: scale(-2);');
+		}else if(this.id != 'base'){
+			moon.setAttribute('style', 'transform: scale(2);');
+		}
+		  
+		const negValue = (this.id === 'base' ? amount : amount * -1);
+        const suffix = (this.id === 'base' ? '' : 's');
+        document.documentElement.style.setProperty(`--${this.id}`, negValue + suffix);
+}// get the inputs
+      const inputs = [].slice.call(document.querySelectorAll('.controls input'));
+
+      // listen for changes
+      inputs.forEach(input => input.addEventListener('change', handleUpdate));
+      inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+      function handleUpdate(e) {
+		var moon = document.getElementById('adjustMoon');
+        // append 'px' to the end of spacing and blur variables
+		var amount = this.value;
+		if(amount > 50 && this.id != 'base'){
+			amount = amount - ((amount - 50) * 2)
+			
+			moon.setAttribute('style', 'transform: scale(-2);');
+		}else if(this.id != 'base'){
+			moon.setAttribute('style', 'transform: scale(2);');
+		}
+		  
+		const negValue = (this.id === 'base' ? amount : amount * -1);
+        const suffix = (this.id === 'base' ? '' : 's');
+        document.documentElement.style.setProperty(`--${this.id}`, negValue + suffix);
+}// get the inputs
+      const inputs = [].slice.call(document.querySelectorAll('.controls input'));
+
+      // listen for changes
+      inputs.forEach(input => input.addEventListener('change', handleUpdate));
+      inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+      function handleUpdate(e) {
+		var moon = document.getElementById('adjustMoon');
+        // append 'px' to the end of spacing and blur variables
+		var amount = this.value;
+		if(amount > 50 && this.id != 'base'){
+			amount = amount - ((amount - 50) * 2)
+			
+			moon.setAttribute('style', 'transform: scale(-2);');
+		}else if(this.id != 'base'){
+			moon.setAttribute('style', 'transform: scale(2);');
+		}
+		  
+		const negValue = (this.id === 'base' ? amount : amount * -1);
+        const suffix = (this.id === 'base' ? '' : 's');
+        document.documentElement.style.setProperty(`--${this.id}`, negValue + suffix);
+}// get the inputs
+      const inputs = [].slice.call(document.querySelectorAll('.controls input'));
+
+      // listen for changes
+      inputs.forEach(input => input.addEventListener('change', handleUpdate));
+      inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+      function handleUpdate(e) {
+		var moon = document.getElementById('adjustMoon');
+        // append 'px' to the end of spacing and blur variables
+		var amount = this.value;
+		if(amount > 50 && this.id != 'base'){
+			amount = amount - ((amount - 50) * 2)
+			
+			moon.setAttribute('style', 'transform: scale(-2);');
+		}else if(this.id != 'base'){
+			moon.setAttribute('style', 'transform: scale(2);');
+		}
+		  
+		const negValue = (this.id === 'base' ? amount : amount * -1);
+        const suffix = (this.id === 'base' ? '' : 's');
+        document.documentElement.style.setProperty(`--${this.id}`, negValue + suffix);
+}
