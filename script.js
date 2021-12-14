805f0840fb4d3a3f9be38e71255683224d306e6c
morsej <90578267+morsej@users.noreply.github.com>
2021-12-14 07:40:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// get the inputs
+      const inputs = [].slice.call(document.querySelectorAll('.controls input'));
+
+      // listen for changes
+      inputs.forEach(input => input.addEventListener('change', handleUpdate));
+      inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+      function handleUpdate(e) {
+        // append 'px' to the end of spacing and blur variables
+        const suffix = (this.id === 'base' ? '' : 'px');
+        document.documentElement.style.setProperty(`--${this.id}`, this.value + suffix);
+      }
